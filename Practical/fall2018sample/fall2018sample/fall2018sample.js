624e77dd7fa26b99d583856930a8b8375d6d6207
Ethan Henson (RIT Student) <esh9426@ad.rit.edu>
2018-11-29 15:18:23 -0500

    Almost done, need to add name

---
+++ Practical/fall2018sample/fall2018sample/fall2018sample.js
@@ -0,0 +1,18 @@
+/* Don't use <script> tags in a linked js file! */
+
+// JS Menu
+$(".menuitem").click(function () {
+    $(this).next(".submenu").toggle(250);
+});
+
+// Ajax Content
+
+
+let Content = ("content1.txt")
+$("#choose-content").val(Content);
+$("#content").load(Content);
+
+$("#choose-content").change(function () {
+    Content = $(this).val();
+    $("#content").load(Content);
+});
